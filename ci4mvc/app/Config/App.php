9687c6901809c735bc85ad3a1ba0a39295156daa
Dumitru Alin <dumitru.mihailalin@gmail.com>
2022-10-30 00:23:57 +0300

    update

--- ci4mvc/app/Config/App.php
+++ ci4mvc/app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost:8080/';
+    public $baseURL = 'http://164.92.141.219/public';
 
     /**
      * --------------------------------------------------------------------------
