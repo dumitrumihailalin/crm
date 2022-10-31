8596ec165139d606cf1a8a942dc7472de4c8cf7f
Dumitru Alin <dumitru.mihailalin@gmail.com>
2022-10-31 18:02:18 +0200

    update

--- ci4mvc/app/Config/App.php
+++ ci4mvc/app/Config/App.php
@@ -109,7 +109,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $appTimezone = 'Bucharest/Europe';
+    public $appTimezone = 'America/Chicago';
 
     /**
      * --------------------------------------------------------------------------
