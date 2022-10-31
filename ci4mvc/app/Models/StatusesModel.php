5ca410754f4e4feee2c35f055cbaa5b398ce4f61
Dumitru Alin <dumitru.mihailalin@gmail.com>
2022-10-31 18:09:08 +0200

    update

--- ci4mvc/app/Models/StatusesModel.php
+++ ci4mvc/app/Models/StatusesModel.php
@@ -16,7 +16,7 @@ class StatusesModel extends \CodeIgniter\Model
     {
         $db = db_connect();
         $query =  $db->query('SELECT statuses.status_id, statuses.status_name, count(devices.device_status) as count_devices 
-                    FROM `statuses` LEFT JOIN devices ON statuses.status_id = devices.device_status GROUP BY statuses.status_id');
+                    FROM `statuses` LEFT JOIN devices ON statuses.status_id = devices.device_status GROUP BY statuses.status_id ORDER BY  statuses.status_id desc');
         return $query->getResultObject();
     }
 }
